f4a00392a9f485d4cc5f5f4d6638866ae208df5d
Marvin <marvin1ronal@gmail.com>
2020-10-06 17:46:37 -0600

    Insertar

--- Lista/src/lista/ListaEnlazada.java
+++ Lista/src/lista/ListaEnlazada.java
@@ -10,5 +10,27 @@ package lista;
  * @author marvi
  */
 public class ListaEnlazada {
-    
+
+    Persona inicial;
+    Persona ultimo;
+
+    public ListaEnlazada() {
+        inicial = null;
+        ultimo = null;
+    }
+
+    public void insertar(Persona nueva) {
+        if (inicial == null) {
+            inicial = nueva;
+            ultimo = nueva;
+        } else {
+            Persona aux = ultimo;
+            aux.setSiguient(nueva);
+            ultimo = nueva;
+        }
+    }
+    public void imprimirLista(){
+        
+    }
+
 }
